ada9ef08c14d9b5e368657bd9afef9ad38995429
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-03-12 13:30:29 +0100

    übungen unterricht

--- src/linkedList/MusicalChairsGame.java
+++ src/linkedList/MusicalChairsGame.java
@@ -7,8 +7,17 @@ public class MusicalChairsGame {
 		MusicalChairs mc1  = new MusicalChairs("Leo", "Max", "Paul", "Haid");
 		MusicalChairs mc2  = new MusicalChairs("Jonas", "Hund");
 		
+//		System.out.println(mc1);
+//		System.out.println(mc2);
+//		mc1.rotate(2);
+//		System.out.println(mc1);
+//		mc1.rotateAndRemoveLast(2);
+//		System.out.println(mc1);
+//		System.out.println();
 		System.out.println(mc1);
-		System.out.println(mc2);
+		mc1.play();
+		System.out.println(mc1);
+		
 
 	}
 
