dc9c81b75cd78bc3895a62754e74b0296dc7463e
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-03-12 12:53:26 +0100

    new exercise

---
+++ src/linkedList/MusicalChairsGame.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class MusicalChairsGame {
+
+	public static void main(String[] args) {
+		
+		MusicalChairs mc1  = new MusicalChairs("Leo", "Max", "Paul", "Haid");
+		MusicalChairs mc2  = new MusicalChairs("Jonas", "Hund");
+		
+		System.out.println(mc1);
+		System.out.println(mc2);
+
+	}
+
+}
