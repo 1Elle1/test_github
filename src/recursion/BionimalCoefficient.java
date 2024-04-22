82f7b0268a2a586a04253c408a0d76f2d8a47c31
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-04-22 15:55:41 +0200

    update

--- src/recursion/BionimalCoefficient.java
+++ src/recursion/BionimalCoefficient.java
@@ -3,10 +3,10 @@ package recursion;
 public class BionimalCoefficient {
 	
 	public static int amount(int n, int k) {
-		if(n==0 | n==k) {
+		if(k==0 | k==n) {
 			return 1;
 		}else {
-			return amount(n-1, k+1)+amount(n-1, k);
+			return amount(n-1, k-1)+amount(n-1, k);
 		}
 	}
 
