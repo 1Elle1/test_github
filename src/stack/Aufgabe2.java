2e74bb280642fe7338b6a7cba5a9be8f846b3ac6
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-04-29 15:10:40 +0200

    correct2

--- src/stack/Aufgabe2.java
+++ src/stack/Aufgabe2.java
@@ -4,48 +4,37 @@ import java.util.LinkedList;
 import java.util.regex.Pattern;
  
 public class Aufgabe2 {
- 
-	private static final Pattern klammern = Pattern.compile(
-			"(|)|[|]|{|}"
-		);
 	
-	
-	public static void main(String[] args) {
-		
-		
- 
+	public static boolean checkBrackets(String input) {
+		LinkedList<Character> stack = new LinkedList<>();
+		for(char c : input.toCharArray()) {
+			if(c == '(' || c == '[' || c== '{') {
+				stack.push(c);
+			}else if (c == ')' || c == ']' || c== '}') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char lastOpen = stack.peek();
+				if((c == ')' && lastOpen != '(') || 
+					(c == ']' && lastOpen != '[') ||
+					(c == '}' && lastOpen != '{')){
+						return false;
+					}
+					stack.pop();
+			}
+		}
+		return stack.isEmpty();
 	}
 	
 	
-	private static boolean sameNumberOfClosedBrackets(String input) {
-		LinkedList<Character> brackets = new LinkedList<>();
-		char bracketNow ="";
-        for (int i = 0; i < input.length(); i++) {
-        	if(input.matches(klammern)) {
-        		brackets.push(input.charAt(i));
-        		char bracketNow ="";
-        	}
-        	if(brackets.getLast().equals(bracketNow)) {
-        		brackets.pop();
-        	}
-        	
-        }
+	
+public static void main(String[] args) {
+	
+	String str = "(())";
+	System.out.println(checkBrackets(str));
+    	
         
-        String reversed = "";
-        while (!brackets.isEmpty()) {
-        	reversed += brackets.pop();
-        }
-		return;		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    }
  
+	
 }
