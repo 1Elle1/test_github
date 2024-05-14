4c76d560074739e0271943b168be3a9babe76f52
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-05-14 09:35:28 +0200

    commit

---
+++ src/StringStringBuilder/Aufgabe2SumOfTheDigits.java
@@ -0,0 +1,17 @@
+package StringStringBuilder;
+
+public class Aufgabe2SumOfTheDigits {
+	
+	public static  int digitSum(long value) {
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
